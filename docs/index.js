c182e924e9930ede0801e7db88aed9fe7ed3a224
Victoria Firsanova <70071046+vifirsanova@users.noreply.github.com>
2021-03-20 20:57:54 +0300

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,63 @@
-class HelloMessage extends React.Component {
+class Square extends React.Component {
   render() {
     return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
       <div>
-        Hello {this.props.name}
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
 }
 
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
 ReactDOM.render(
-  <HelloMessage name="pug" />,
-  document.getElementById('hello-example')
+  <Game />,
+  document.getElementById('root')
 );
