c26e6947db4a3a464387c9b277cf0d9580d3298e
Виктория Фирсанова <vifirsanova@gmail.com>
2021-03-30 23:54:15 +0300

    upgate all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useReducer, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+	  ...state,
+	  [event.name]: event.value
+    }
+  }
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = event => {
+	  event.preventDefault();
+	  setSubmitting(true);
+	  
+	  setTimeout(() => {
+		  setSubmitting(false);
+	  }, 3000)
+	  
+	  alert('Приятно познакомиться!')
+}
+
+const handleChange = event => {
+  setFormData({
+    name: event.target.name,
+    value: event.target.value,
+  });
+}
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +45,26 @@ function App() {
         >
           Страница проекта на Open Data Science
         </a>
+		<p> Давайте познакомимся </p>
+		{submitting &&
+		<div> 
+			<ul>
+				{Object.entries(formData).map(([name, value]) => (
+				<a key={name}>Приятно познакомиться, {value.toString()}</a>
+				))}
+			</ul>
+		</div>
+		}
+		<form onSubmit={handleSubmit}>
+		<fieldset>
+			<label>
+				<p>Введите Ваше имя</p>
+				<input name="name" onChange={handleChange}/>
+			</label>
+		</fieldset>
+		<button type="submit">Отправить</button>
+		</form>	
+			
       </header>
     </div>
   );
