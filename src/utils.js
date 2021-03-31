d0a2a7077aebea82c926953353a86203fadc199e
Victoria Firsanova <70071046+vifirsanova@users.noreply.github.com>
2021-03-31 22:44:08 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-const ENDPOINT_URL = "http://localhost:5000/asd_qa";
+const ENDPOINT_URL = "https://vifirsanova.github.io/asd_qa/";
 
 const useAsyncEndpoint = fn => {
     const [res, setRes] = React.useState({ data: null, complete: false, pending: false, error: false });
